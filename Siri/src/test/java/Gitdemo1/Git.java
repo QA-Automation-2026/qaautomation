f6a96d94db56d808c85c41d684ac4d0881a28196
12234556 <saguturusiri@gmail.com>
2026-01-06 12:00:01 -0500

    1/6/2025 operaters

--- Siri/src/test/java/Gitdemo1/Git.java
+++ Siri/src/test/java/Gitdemo1/Git.java
@@ -6,7 +6,16 @@ public class Git {
     public static void main(String[] args) {
 
 //
+      /* git status
+      git add file name
+      git commit -m "commit messages"
+      git push origin Siri
+
+      *
+      * */
 int a = 20;
+
+
 
     }
 }
