f011fb390bafb25026e12895c5e749d6ca4e9751
aniladavi <anil.adavi@gmail.com>
2026-01-06 08:15:46 +0530

    consturctor with parameters

---
+++ Aniladavi/src/test/java/com/git/Package/constructorMethod.java
@@ -0,0 +1,31 @@
+package com.git.Package;
+
+public class constructorMethod {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        String s1 = "Anil";
+        String s2 = "Sagar";
+
+
+        constructorMethod np = new constructorMethod ();
+        constructorMethod add = new constructorMethod (a,b);
+        constructorMethod strcon = new constructorMethod (s1,s2);
+
+    }
+
+    public constructorMethod ()
+    {
+        System.out.println("No parameters");
+    }
+    public constructorMethod (int one, int two)
+    {
+        System.out.println("added " + (one+two));
+    }
+    public constructorMethod (String one, String two)
+    {
+        System.out.println("concat " + (one + "+" +two));
+    }
+}
