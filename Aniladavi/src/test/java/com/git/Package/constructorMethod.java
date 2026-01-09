dca302521768e8851e2dac0478ddc304f4755e43
aniladavi <anil.adavi@gmail.com>
2026-01-09 21:55:43 +0530

    constructors updated on 9th Jan

--- Aniladavi/src/test/java/com/git/Package/constructorMethod.java
+++ Aniladavi/src/test/java/com/git/Package/constructorMethod.java
@@ -8,24 +8,29 @@ public class constructorMethod {
 
         String s1 = "Anil";
         String s2 = "Sagar";
+        String s3 = "Vidya";
 
-        constructorMethod np = new constructorMethod ();
+       /* constructorMethod np = new constructorMethod ();*/
         constructorMethod add = new constructorMethod (a,b);
-        constructorMethod strcon = new constructorMethod (s1,s2);
+       /* constructorMethod strcon = new constructorMethod (s1,s2);
         constructorMethod strle = new constructorMethod (s1,a);
         constructorMethod strlen1 = new constructorMethod ("test1");
         constructorMethod strlen2 = new constructorMethod ("test2");
-
-
+        constructorMethod strlen3 = new constructorMethod (s1,s2,s3);*/
     }
 
     public constructorMethod ()
     {
         System.out.println("No parameters");
     }
-    public constructorMethod (int one, int two)
+    public constructorMethod (int text1, int text2)
     {
-        System.out.println("added " + (one+two));
+        System.out.println("added " + (text1+text2));
+        if (text1 <= text2) {
+            for (int i = text1; i<= text2; i++) {
+                System.out.println(("For loop" + i));
+            }
+        }
     }
     public constructorMethod (String one, String two)
     {
@@ -38,5 +43,10 @@ public class constructorMethod {
     public constructorMethod (String one)
     {
         System.out.println("String is " + one);
+    }
+
+    public constructorMethod (String one, String two, String three)
+    {
+        System.out.println("concat " + (one + "+" +two + "+" + three));
     }
 }
