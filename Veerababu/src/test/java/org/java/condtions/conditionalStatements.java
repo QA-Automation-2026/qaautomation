e9aa1249c98c25b65c11370e4c2906ab0f1ecd27
QA-Automation-2026 <veerababumsg@gmail.com>
2026-01-11 21:27:11 +0530

    commit today

--- Veerababu/src/test/java/org/java/condtions/conditionalStatements.java
+++ Veerababu/src/test/java/org/java/condtions/conditionalStatements.java
@@ -20,23 +20,25 @@ public class conditionalStatements {
         int a = 10;
         int b = 20;
         int c = 50;
-        if(!(((a>c)&& (c<=b))||(a<c))){
+        if(!(((a>c)&& (c<=b))||(a<c)))
+        {
             System.out.println("a is greater than b - Siri");
         }else{
             System.out.println("b is greater than a - Veerababu");
         }
-
-        if(a/b==0){ // / means reminder value
+        if(a/b==0)
+        { // / means reminder value
             System.out.println("a is even number");
         }else{
             System.out.println("a is not an even number");
         }
         char ch = 'e';
-        if(ch=='a'|| ch == 'e'|| ch == 'i'||ch == 'o'|| ch == 'u'){
+        if(ch=='a'|| ch == 'e'|| ch == 'i'||ch == 'o'|| ch == 'u')
+        {
             System.out.println("e is a vowel");
-        }else{
+        }else
+        {
             System.out.println("d is a consonant");
         }
-
     }
 }
