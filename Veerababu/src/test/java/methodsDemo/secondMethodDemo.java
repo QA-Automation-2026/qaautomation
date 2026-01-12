6b2deed79e66826a285037f0314b14745e3fd30d
QA-Automation-2026 <veerababumsg@gmail.com>
2026-01-12 12:18:37 +0530

    today by veerababu

---
+++ Veerababu/src/test/java/methodsDemo/secondMethodDemo.java
@@ -0,0 +1,12 @@
+package methodsDemo;
+
+public class secondMethodDemo extends  methodsSession{
+
+    public static void main(String[] args) {
+        methodsSession obj = new methodsSession();
+        obj.firstMethod();
+        obj.secondMethod();
+        obj.thirdMethod();
+        obj.fourthMethod();
+    }
+}
