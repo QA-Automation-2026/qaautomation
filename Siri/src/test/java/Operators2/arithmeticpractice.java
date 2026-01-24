72abfcc8745b6e20097d7fb37e8735d532d6a6ae
12234556 <saguturusiri@gmail.com>
2026-01-24 13:44:20 -0500

    see the changes

--- Siri/src/test/java/Operators2/arithmeticpractice.java
+++ Siri/src/test/java/Operators2/arithmeticpractice.java
@@ -10,12 +10,14 @@ public class arithmeticpractice {
         int a = 10;
         int b=20;
         int c =30;
+        int d =50;
 
-        System.out.println("addition of two numbwrs "+(a+b));
-        System.out.println("substrct if two numbers  "+(a-c));
+        System.out.println("addition of two numbers "+(a+b));
+        System.out.println("subtract if two numbers  "+(a-c));
         System.out.println("divided if two numbers" + (a%c));
         System.out.println("multiplied by two numbers"+(b*c));
         System.out.println("addition of three numbers"+(a+b+c));
+        System.out.println("celsiustofahrenheit of two numbers"+(d*9/b));
     }
 
 }
