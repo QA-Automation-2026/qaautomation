f3176b35ad1d02a07b297b9d3c939d1eb8a7e64b
12234556 <saguturusiri@gmail.com>
2026-01-23 21:36:27 -0500

    plz check

--- Siri/src/test/java/Javaclass/variablespractice1.java
+++ Siri/src/test/java/Javaclass/variablespractice1.java
@@ -6,23 +6,24 @@ public class variablespractice1 {
    static int b=20;
    static int c=30;
 
-    public static void main(String[] args) {//global
-        variablespractice1 obj=new variablespractice1();
-        System.out.println("variablespractice1+a>=b");//global and static
-        System.out.println("b>a");
-        System.out.println("a<b");
-        System.out.println("a+b>c");
-        System.out.println("a+b=c");
+    public static void main(String[] args) {
+        variablespractice1 obj=new variablespractice1();//global
+        System.out.println(obj.a+b);//global and static
+        System.out.println(obj.b-a);
+        System.out.println(obj.a%b);
+        System.out.println(obj.a/b);
+        System.out.println(obj.a+c);
 
-        System.out.println("a and b");//static
-        System.out.println("a");
+        System.out.println(a+b);
+        System.out.println(a);
 
         int a= 40;//local
         int b=50;
-        System.out.println("a<b");
-        System.out.println("b>a");
-        System.out.println("50");
-        System.out.println("a+a+b");//static and local
+        System.out.println(a<b);
+        System.out.println(b>a);
+        System.out.println(50);
+        System.out.println(b);
+        System.out.println(a+a+b);//static and local
 
 
     }
