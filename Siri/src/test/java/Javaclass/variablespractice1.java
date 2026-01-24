e790939d4eac10b970d1cb4ecfddaca654a73a84
12234556 <saguturusiri@gmail.com>
2026-01-24 12:47:19 -0500

    Siri

--- Siri/src/test/java/Javaclass/variablespractice1.java
+++ Siri/src/test/java/Javaclass/variablespractice1.java
@@ -5,13 +5,15 @@ public class variablespractice1 {
    static int a =10;
    static int b=20;
    static int c=30;
+       int d= 40;
+       int e =60;
 
     public static void main(String[] args) {
         variablespractice1 obj=new variablespractice1();//global
         System.out.println(obj.a+b);//global and static
-        System.out.println(obj.b-a);
-        System.out.println(obj.a%b);
-        System.out.println(obj.a/b);
+        System.out.println(obj.d);//global
+        System.out.println(a%b);//static
+        System.out.println(obj.d+obj.e);//global
         System.out.println(obj.a+c);
 
         System.out.println(a+b);
@@ -23,7 +25,7 @@ public class variablespractice1 {
         System.out.println(b>a);
         System.out.println(50);
         System.out.println(b);
-        System.out.println(a+a+b);//static and local
+        System.out.println( variablespractice1.a+a+b);//static and local
 
 
     }
