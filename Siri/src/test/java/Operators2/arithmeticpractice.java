209d3a7622d16863b4552e3aff43fcf52549638c
12234556 <saguturusiri@gmail.com>
2026-01-24 13:27:05 -0500

    Arthemeticpractice 1/24/2026

---
+++ Siri/src/test/java/Operators2/arithmeticpractice.java
@@ -0,0 +1,21 @@
+package Operators2;
+
+public class arithmeticpractice {
+    int a = 10;
+    int b=20;
+    int c =30;
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b=20;
+        int c =30;
+
+        System.out.println("addition of two numbwrs "+(a+b));
+        System.out.println("substrct if two numbers  "+(a-c));
+        System.out.println("divided if two numbers" + (a%c));
+        System.out.println("multiplied by two numbers"+(b*c));
+        System.out.println("addition of three numbers"+(a+b+c));
+    }
+
+}
