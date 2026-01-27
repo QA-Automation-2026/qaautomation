406abba6c1997dd8db0d84b1df7e2671716a93f4
12234556 <saguturusiri@gmail.com>
2026-01-26 19:32:37 -0500

    check operators

---
+++ Veerababu/src/test/java/revision/logicalOparators.java
@@ -0,0 +1,17 @@
+package revision;
+
+public class logicalOparators {
+    int age =20;
+    int b=18;
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        System.out.println(" age is eligible for the job"+(age >= 18 && age >=20));
+        System.out.println("person below 18 not eligible for the job"+(b<18));
+        System.out.println( "a is vowel"+
+         (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'));
+
+
+    }
+}
+
