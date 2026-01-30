3e0855ed624074f5f01b3cb110efaaf51235ae08
12234556 <saguturusiri@gmail.com>
2026-01-29 20:44:52 -0500

    terinary Operators

---
+++ Siri/src/test/java/Operators2/terinaryOperators.java
@@ -0,0 +1,29 @@
+package Operators2;
+
+public class terinaryOperators {
+
+
+    public static void main(String[] args) {
+
+
+        int d =40;
+        int a =30;
+        d= 40;
+        int c1=(d>a)? d:a;
+        System.out.println(c1);
+
+    }
+
+
+}
+/*
+*  // Terinary operator
+        //condition ? true : false
+        int i = 10;
+        j = 15;
+        int c1 = (i<j)?i:j;
+        System.out.println(c1);
+
+*
+*
+* */
