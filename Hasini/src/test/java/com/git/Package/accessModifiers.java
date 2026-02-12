c39a996054d4d0f4b448542162c0f602ef9405b7
aniladavi <anil.adavi@gmail.com>
2026-02-12 21:22:57 +0530

    12 th feb 2026 - overload/override

---
+++ Hasini/src/test/java/com/git/Package/accessModifiers.java
@@ -0,0 +1,15 @@
+package com.git.Package;
+
+public class accessModifiers {
+
+    public static void main (String[] arg)
+    {
+        int i = test1();
+        System.out.println(i);
+    }
+
+    public static int test1() {
+        return 10;
+
+    }
+}
---
+++ Hasini/src/test/java/com/git/Package/parent.java
@@ -0,0 +1,17 @@
+package com.git.Package;
+
+public class parent {
+
+    public void test()
+    {
+
+        System.out.println("parent class");
+    }
+    public void test4()
+    {
+
+        System.out.println("parent class 4");
+    }
+
+
+}
