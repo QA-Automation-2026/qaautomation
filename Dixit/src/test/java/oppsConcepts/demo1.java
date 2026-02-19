f45877309b5f1c7bbf68cc5814a9050a9cbb31ee
Dixitsunny <dixit.sunny229@gmail.com>
2026-02-19 23:13:37 +0530

    interface classes and 19-02

--- Dixit/src/test/java/oppsConcepts/demo1.java
+++ Dixit/src/test/java/oppsConcepts/demo1.java
@@ -1,21 +1,22 @@
 package oppsConcepts;
 
-public class demo1 extends oppsDemo{
+public class demo1 extends oppsDemo {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         oppsDemo obj = new oppsDemo();
-        int marks =function1(52,65);
+        int marks = function1(52, 65);
         System.out.println(marks);
     }
 
-    public static int differentParametersFunction(int a, int b){
-        if(a>b) {
+    public static int differentParametersFunction(int a, int b) {
+        if (a > b) {
             return a;
-        }else {
+        } else {
             return b;
         }
     }
-    public static String differentParametersFunction(String name ){
+
+    public static String differentParametersFunction(String name) {
         return "";
     }
 }
