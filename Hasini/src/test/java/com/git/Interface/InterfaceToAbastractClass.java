7a9394abe1c9ad8795a7dac80169644ef91006d9
aniladavi <anil.adavi@gmail.com>
2026-02-21 12:06:25 +0530

    21 feb interace practise

---
+++ Hasini/src/test/java/com/git/Interface/InterfaceToAbastractClass.java
@@ -0,0 +1,8 @@
+package com.git.Interface;
+
+public abstract class InterfaceToAbastractClass implements InterfacePractise{
+    public void testm1()
+    {
+        System.out.println("testm1 from abstract class implemented from interface");
+    }
+}
